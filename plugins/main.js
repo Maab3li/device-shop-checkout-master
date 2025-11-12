064a90e74abc8ddfb9d9c2863ff45dc3624628f2
Maab3li <maab3li555@gmail.com>
2025-11-12 09:00:14 -0800

    first commit

---
+++ plugins/main.js
@@ -0,0 +1,14 @@
+var countries = [];
+var selectElement = document.getElementById('countries');
+fetch('/assets/data/countries.json')
+    .then(function (response) { return response.json(); })
+    .then(function (data) {
+    countries = data;
+    countries.forEach(function (country) {
+        var option = document.createElement('option');
+        option.textContent = country.name;
+        option.value = country.code;
+        selectElement === null || selectElement === void 0 ? void 0 : selectElement.appendChild(option);
+    });
+})
+    .catch(function (error) { throw new Error('Failed to fetch countries', error); });
---
+++ plugins/main.ts
@@ -0,0 +1,18 @@
+let countries: Array<object> =[];
+
+const selectElement = document.getElementById('countries')
+
+    fetch('/assets/data/countries.json')
+    .then(response => response.json())
+    .then(data => {
+        countries = data
+        countries.forEach(country => {
+        const option = document.createElement('option')
+        option.textContent = country.name
+        option.value = country.code
+        selectElement?.appendChild(option)
+        })
+    })
+    .catch(error => {throw new Error('Failed to fetch countries', error)})
+    
+    
